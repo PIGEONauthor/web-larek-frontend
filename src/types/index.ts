e40a6d137e14ddae88ce1701221aaf4b1ce9a695
Alexey <funtic1991@gmail.com>
2024-01-20 21:49:39 +0300

    arch 1.1

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
+//------------------------------EVENT EMITTER------------------------------
+
 export type EventName = string | RegExp;
 
 export interface IEvents {
@@ -5,6 +7,8 @@ export interface IEvents {
   emit<T extends object>(event: string, data?: T): void;
   trigger<T extends object>(event: string, context?: Partial<T>): (data: T) => void;
 }
+
+//------------------------------VIEW------------------------------
 
 export interface IView<
   NodeType extends HTMLElement,
@@ -39,6 +43,8 @@ export interface IView<
   mount(selectorElement: HTMLElement | string, data?: any, name?: string): any;
 }
 
+//------------------------------API------------------------------
+
 export interface IAPI {
   baseUrl: string;
   options: RequestInit;
@@ -47,18 +53,18 @@ export interface IAPI {
   post(uri: string, data: object, method: string): Promise<object>;
 }
 
+//------------------------------BUTTON------------------------------
+
 export interface IButton {
   label: string;
-  events: 'click' | 'submit'
+  events: 'click' | 'submit';
+
+  setLabel(label: string): void;
+  enable(): void;
+  disable(): void;
 }
 
-export type GalleryItem = IView<HTMLElement, object, 'click', string>;
-
-export interface IGallery {
-  items: GalleryItem[];
-
-  setItems(items: IView<HTMLElement, object, 'click', string>[]): void;
-}
+//------------------------------MODAL------------------------------
 
 export interface IModal {
   container: HTMLElement;
@@ -71,6 +77,8 @@ export interface IModal {
   reset(): void;
 }
 
+//------------------------------FORM------------------------------
+
 export interface IForm {
   container: HTMLElement;
   submit: HTMLButtonElement;
@@ -78,4 +86,138 @@ export interface IForm {
 
   isValid(): boolean;
   setErrors(error: string): void;
+}
+
+//------------------------------CARD DATA------------------------------
+
+export type CardData = {
+  description: string;
+  image: string;
+  title: string;
+  category: string;
+  price: string | number
+};
+
+export type CardID = string;
+
+export interface ICardsData {
+  cards: Map<CardID, CardData>
+
+  addCard(id: CardID, data: CardData): void;
+  getCard(id: CardID): CardData;
+  getCards(): CardData[];
+}
+
+//------------------------------BUSKET DATA------------------------------
+
+export type Order = {
+  number: number;
+  title: string;
+  price: string
+}
+
+export interface IBusketData {
+  orders: Order[];
+
+  addOrder(order: Order): void;
+  getOrders(): Order[];
+  getCount(): number;
+}
+
+//------------------------------GALLERY------------------------------
+
+export type GalleryItem = IView<HTMLElement, object, 'click', string>;
+
+export interface IGallery {
+  items: GalleryItem[];
+
+  setItems(items: IView<HTMLElement, object, 'click', string>[]): void;
+}
+
+//------------------------------PAGE------------------------------
+
+export interface IPage {
+  counter: number;
+  gallery: GalleryItem[];
+
+  lockScroll(): void;
+  openBusket(): void
+}
+
+//------------------------------CARD------------------------------
+
+export interface ICard {
+  id: string;
+  category: string;
+  title: string;
+  image: string;
+  price: string;
+
+  setImage(link: string): void;
+  setTitle(text: string): void;
+  setCategory(category: string): void;
+  setPrice(price: string): void;
+  openModal(modal: HTMLElement, id: string): void
+}
+
+//------------------------------PRODUCT------------------------------
+
+export interface IProduct extends ICard {
+  description: string;
+
+  setDescription(text: string): void;
+  addToBasket(): void;
+  openBasket(): void;
+}
+
+//------------------------------BUSKET------------------------------
+
+export interface IBusket {
+  title: string;
+  ordersList: HTMLElement[];
+  button: HTMLButtonElement;
+  total: string;
+
+  setTitle(title: string): void;
+  setOrders(orders: HTMLElement[]): void;
+  remove(order: HTMLElement): void;
+  getSum(): string;
+  accept(): void;
+}
+
+//------------------------------PAYMENT------------------------------
+
+export interface IPayment {
+  title: string;
+  formContent: HTMLElement;
+  button: HTMLButtonElement;
+
+  getAddress(addr: HTMLInputElement): string;
+  isValide(): boolean;
+  getValidationMessage(): string;
+  accept(): void
+}
+
+//------------------------------USERDATA------------------------------
+
+export interface IUserData {
+  title: string;
+  formContent: HTMLElement;
+
+  getValue(value: HTMLInputElement): string;
+  isValid(): boolean;
+  getValidationMessage(): string;
+  accept(): void
+}
+
+//------------------------------SUCCESS------------------------------
+
+export interface ISuccess {
+  image: string;
+  description: string;
+  button: HTMLButtonElement;
+
+  setImage(link: string): void;
+  setDescription(text: string): void;
+  accept(): void
 }
